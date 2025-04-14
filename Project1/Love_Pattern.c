a5761efe64fde4961df7e6f0bf2d415a839eed28
MapleGo <109315289+sweetmaple@users.noreply.github.com>
2025-04-14 23:21:24 +0800

    Add files via upload

---
+++ Project1/Love_Pattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//Love pattern
+int main() {
+    printf("      **          **\n");
+    printf("   *******      *******\n");
+    printf("  ******I LOVE YOU******\n");
+    printf("  **********************\n");
+    printf("    ******************\n");
+    printf("      **************\n");
+    printf("        **********\n");
+    printf("          *****\n");
+    printf("            *\n");
+    return 0;
+}
