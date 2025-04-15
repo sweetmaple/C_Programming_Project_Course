bd85c627a275ffeb987c3e36338de11a43013337
MapleGo <109315289+sweetmaple@users.noreply.github.com>
2025-04-15 12:17:02 +0800

    Add files via upload

---
+++ Project3/Surface_Area_Of_Sphere.c
@@ -0,0 +1,13 @@
+/* Surface_Area_Of_Sphere */
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14
+int main() {
+    float r;
+    double Area;
+    printf("Please input the radius of the sphere:");
+    scanf("%f", &r);
+    Area = 4 * PI * pow(r, 2); // Area = 4 * PI * r * r;
+    printf("Sphere's surface area is %f\n", Area);
+    return 0;
+}
