3aec675c7b511d641bd00b0f3c2c530f48c33168
MapleGo <109315289+sweetmaple@users.noreply.github.com>
2025-04-18 14:30:08 +0800

    Update Less_Than_Or_Equal_10000.c

--- Project5/Less_Than_Or_Equal_10000.c
+++ Project5/Less_Than_Or_Equal_10000.c
@@ -5,7 +5,7 @@ int main() {
     for (n = 1; s <= 10000; n++) {
         s += n;
     }
-    printf("n = %d\n", n - 2);
+    printf("n = %d\n", n - 2); // n - 2的原因是什么? 假设 n = 100, s = 9999; n = 101, s = 19999; n = 102, 不进入循环
     return 0;
 }
 
