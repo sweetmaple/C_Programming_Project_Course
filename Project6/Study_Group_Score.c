e720105c5eef5125d22c929242509c45421e93e1
MapleGo <109315289+sweetmaple@users.noreply.github.com>
2025-05-14 01:30:31 +0800

    Update Study_Group_Score.c

--- Project6/Study_Group_Score.c
+++ Project6/Study_Group_Score.c
@@ -3,18 +3,15 @@
 int main() {
     float form[5][3] = {{90, 93, 89}, {92, 95, 95}, {85, 78, 80},
                         {78, 82, 80}, {81, 83, 86}};
-    float students[5];
+    float students_score[5];
     float sum = 0, avg = 0;
     int i;
     for (i = 0; i < 5; i++) {
-        students[i] = form[i][0] * 0.2 + form[i][1] * 0.2 + form[i][2] * 0.6;
+        students_score[i] = form[i][0] * 0.2 + form[i][1] * 0.2 + form[i][2] * 0.6;
+        sum += students_score[i];
     }
     printf("LiuXiao:%f\nAiLin:%f\nWeiWei:%f\nLinChen:%f\nHanYang:%f\n",
-           students[0], students[1], students[2], students[3], students[4]);
-    for (i = 0; i < 5; i++) {
-        sum += students[i];
-    }
-
+           students_score[0], students_score[1], students_score[2], students_score[3], students_score[4]);
     avg = sum / 5;
     printf("The average score of this group:%f\n", avg);
     return 0;
